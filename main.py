5b406ab743b67d6878e766b6ecfc80a270c2676e
Suveshan <suveshan.moodley@gmail.com>
2024-05-26 06:10:30 +0200

    Basic discord bot setup without the .env file for safety.

---
+++ main.py
@@ -0,0 +1,28 @@
+# Install the discord package to import below
+import discord
+from discord.ext import commands
+
+# Install the python-dotenv package to import
+from dotenv import load_dotenv
+from typing import Final
+import os
+
+# Load the bot token from .env file
+load_dotenv()
+TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
+
+# Setup bot to be interactive
+intents = discord.Intents.default()
+intents.message_content = True  # Enable message content intent
+
+# Set the bot command prefix and remove the default help function
+bot = commands.Bot(command_prefix='!', intents=intents)
+bot.remove_command('help')
+
+
+@bot.event # Notification that the bot is ready to use once run
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+
+# Run the bot using the bot token
+bot.run(TOKEN)
